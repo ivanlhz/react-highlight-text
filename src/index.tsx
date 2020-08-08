d7fbb2ff123fa9faa1f6a69448d4a08952225107
Iván López Hdez <ivanlhz@gmail.com>
2020-08-08 14:49:45 +0100

    Remove base component and refactorize

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import React, { FC } from 'react';
 import { HighlightTextProps } from './HighlightText.types';
 
-const baseComponentHighlight = (text: string, highlight: string) => (
+const baseComponentHighlight = (text: string, highlight?: string) => (
   <React.Fragment key={Math.random()}>
     {text}
-    <strong>{highlight}</strong>
+    {highlight && <strong>{highlight}</strong> }
   </React.Fragment>
 );
-
-const baseComponent = (text: string) => <React.Fragment key={Math.random()}>{text}</React.Fragment>;
 
 const HighlightText: FC<HighlightTextProps> = ({ text, highlight }) => {
   const getComponents = (text: string, highlight: string): JSX.Element[] => {
     const splittedText = text.split(highlight);
-    let toReturn: JSX.Element[] = splittedText.map((textFragment, index) =>
-      index < splittedText.length - 1 ? baseComponentHighlight(textFragment, highlight) : baseComponent(textFragment)
+    const toReturn: JSX.Element[] = splittedText.map((textFragment, index) =>
+      index < splittedText.length - 1 ? baseComponentHighlight(textFragment, highlight) : baseComponentHighlight(textFragment)
     );
     return toReturn;
   };
